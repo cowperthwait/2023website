8f21c8b2f711eccc045ad5c5169a3256fda4842f
jwpettit <jwpettit@me.com>
2023-04-20 14:37:40 -0700

    Subheader in place

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,11 @@
 import LandingHeader from "../landing-components/LandingHeader";
+import LandingSubheader from "../landing-components/LandingSubheader";
 
 export default function Landing() {
     return (
         <>
             <LandingHeader />
+            <LandingSubheader />
         </>
     );
 }
