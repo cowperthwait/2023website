21cb83d3a96848aa49fb617f95e8ec30bafe8794
jwpettit <jwpettit@me.com>
2023-04-20 14:59:46 -0700

    Work section of resume in place

--- src/landing-components/LandingSubheader.jsx
+++ src/landing-components/LandingSubheader.jsx
@@ -2,6 +2,7 @@ export default function LandingSubheader() {
     return (
         <div className="Subheader-Container">
             <h2 className="italic">Slow runner, fast talker, good writer</h2>
+            <div className="Spacer-1X" />
             <p className="large">
                 Positioning, messaging, and go-to-market strategy to maximize
                 awareness and revenue for technology brands of all sizes.
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,15 @@
 import LandingHeader from "../landing-components/LandingHeader";
 import LandingSubheader from "../landing-components/LandingSubheader";
+import LandingWorkSection from "../landing-components/LandingWorkSection";
 
 export default function Landing() {
     return (
         <>
             <LandingHeader />
+            <div className="Spacer-2X" />
             <LandingSubheader />
+            <div className="Spacer-4X" />
+            <LandingWorkSection />
         </>
     );
 }
