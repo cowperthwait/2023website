ff8dff4c512758c35c0297d08313159a0582617c
jwpettit <jwpettit@me.com>
2023-04-20 12:17:44 -0700

    Started structuring out app framework

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./assets/fonts/Nov-Medium.ttf";
+import "./assets/fonts/Nov-MediumItalic.ttf";
+import "./assets/fonts/Nov-Regular.ttf";
+import "./assets/fonts/Nov-SemiLightItalic.ttf";
+import "./assets/fonts/TitleBroadOutlineMedium.ttf";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+export default function App() {
+    return (
+        <div className="App">
+            <Routes>
+                <Route></Route>
+            </Routes>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
