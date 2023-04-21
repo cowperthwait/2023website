0e0066e8d523348f546dfce434a7d1429d9869de
jwpettit <jwpettit@me.com>
2023-04-21 13:31:10 -0700

    Fixed button to download resume

--- src/landing-components/LandingCVSection.jsx
+++ src/landing-components/LandingCVSection.jsx
@@ -1,33 +1,17 @@
-import { useState } from "react";
-import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
 import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 
 export default function LandingCVSection() {
-    const [sectionOpen, setSectionOpen] = useState(false);
-
-    const toggleSection = () => {
-        sectionOpen ? setSectionOpen(false) : setSectionOpen(true);
-    };
-
     return (
         <>
-            <h2 onClick={toggleSection} className="Buttonized">
-                CV{" "}
-                {sectionOpen ? (
-                    <KeyboardDoubleArrowDownIcon fontSize="small" />
-                ) : (
-                    <KeyboardDoubleArrowRightIcon fontSize="small" />
-                )}
-            </h2>
-            <div className={`content ${sectionOpen ? "open" : "closed"}`}>
-                {
-                    <>
-                        <div className="Spacer-2X" />
-                        <h3>Figure out how to show this ask Jon</h3>
-                        <div className="Spacer-3X" />
-                    </>
-                }
-            </div>
+            <a
+                href={`${process.env.PUBLIC_URL}/downloadablefiles/Jonathan E Cowperthwait spring 2023 CV.pdf`}
+                download="Jonathan E Cowperthwait spring 2023 CV.pdf"
+                className="Buttonized"
+            >
+                <h2 className="Buttonized">
+                    CV <KeyboardDoubleArrowRightIcon fontSize="small" />
+                </h2>
+            </a>
         </>
     );
 }
