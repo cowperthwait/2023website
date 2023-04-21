f14e245bc4059fb0c2c1f635227a8d83c7db3134
jwpettit <jwpettit@me.com>
2023-04-21 11:32:27 -0700

    All sections animated

--- src/landing-components/LandingCVSection.jsx
+++ src/landing-components/LandingCVSection.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 export default function LandingCVSection() {
+    const [sectionOpen, setSectionOpen] = useState(false);
+
+    const toggleSection = () => {
+        sectionOpen ? setSectionOpen(false) : setSectionOpen(true);
+    };
+
     return (
         <>
-            <h2>CV »</h2>
-            <div className="Spacer-2X" />
-            <h3>Figure out how to show this ask Jon</h3>
+            <h2 onClick={toggleSection} className="Buttonized">
+                CV »
+            </h2>
+            <div className={`content ${sectionOpen ? "open" : "closed"}`}>
+                {
+                    <>
+                        <div className="Spacer-2X" />
+                        <h3>Figure out how to show this ask Jon</h3>
+                        <div className="Spacer-3X" />
+                    </>
+                }
+            </div>
         </>
     );
 }
--- src/landing-components/LandingTalkSection.jsx
+++ src/landing-components/LandingTalkSection.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 export default function LandingTalkSection() {
+    const [sectionOpen, setSectionOpen] = useState(false);
+
+    const toggleSection = () => {
+        sectionOpen ? setSectionOpen(false) : setSectionOpen(true);
+    };
+
     return (
         <>
-            <h2>let's talk »</h2>
+            <h2 onClick={toggleSection} className="Buttonized">
+                let’s talk »
+            </h2>
+            <div className={`content ${sectionOpen ? "open" : "closed"}`}>
+                {
+                    <>
+                        <div className="Spacer-2X" />
+
+                        <p>Ask Jon</p>
+
+                        <div className="Spacer-3X" />
+                    </>
+                }
+            </div>
         </>
     );
 }
