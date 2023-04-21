75fdad184f7a50a8fc6b5be3ff3cbfe2fbaf7aa1
jwpettit <jwpettit@me.com>
2023-04-21 09:26:15 -0700

    Responsive adjustment additions to work properly at iPhone scale

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
     return (
-        <div className="Page-Content-Container-1">
+        <>
             <Outlet />
-        </div>
+        </>
     );
 }
