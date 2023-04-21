0cd1e719cc5f15d3a52894d73b92e97604fcd8f8
jwpettit <jwpettit@me.com>
2023-04-21 09:06:18 -0700

    Image moves above name at smaller viewpoints

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,19 @@
 import LandingHeader from "../landing-components/LandingHeader";
 import LandingSubheader from "../landing-components/LandingSubheader";
+import LandingHeadshot from "../landing-components/LandingHeadshot";
 import LandingWorkSection from "../landing-components/LandingWorkSection";
 
 export default function Landing(props) {
     return (
         <div className="Page-Content-Container-1">
+            {props.windowWidth <= 740 ? (
+                <div>
+                    <LandingHeadshot />
+                    <div className="Spacer-1X" />
+                </div>
+            ) : (
+                <></>
+            )}
             <LandingHeader windowWidth={props.windowWidth} />
             <div className="Spacer-2X" />
             <LandingSubheader />
