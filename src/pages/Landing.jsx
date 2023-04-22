eb119f453a4a61fa272d02ffc908323dba4146ad
jwpettit <jwpettit@me.com>
2023-04-22 12:57:29 -0700

    Added SEO metadata to Landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import SEO from "../universal-components/SEO";
 import LandingHeader from "../landing-components/LandingHeader";
 import LandingSubheader from "../landing-components/LandingSubheader";
 import LandingHeadshot from "../landing-components/LandingHeadshot";
@@ -9,35 +10,42 @@ import LandingCVSection from "../landing-components/LandingCVSection";
 import LandingTalkSection from "../landing-components/LandingTalkSection";
 import ReactGA from "react-ga";
 
-
 export default function Landing(props) {
     useEffect(() => {
         ReactGA.pageview(window.location.pathname);
     }, []);
 
     return (
-        <div className="Page-Content-Container-1">
-            {props.windowWidth <= 740 ? (
-                <div>
-                    <LandingHeadshot />
-                    <div className="Spacer-1X" />
-                </div>
-            ) : (
-                <></>
-            )}
-            <LandingHeader windowWidth={props.windowWidth} />
-            <div className="Spacer-2X" />
-            <LandingSubheader />
-            <div className="Spacer-4X" />
-            <LandingWorkSection />
-            <div className="Spacer-1X" />
-            <LandingCausesSection />
-            <div className="Spacer-1X" />
-            <LandingEducationSection />
-            <div className="Spacer-1X" />
-            <LandingCVSection />
-            <div className="Spacer-1X" />
-            <LandingTalkSection />
-        </div>
+        <>
+            <SEO
+                title="Jonathan E Cowperthwait | SF Bay Area Product Marketing Leader"
+                description="Jonathan E Cowperthwait does positioning, messaging, and go-to-market strategy to maximize awareness and revenue for technology brands of all sizes."
+                name="Jonathan E Cowperthwait"
+                type="website"
+            />
+            <div className="Page-Content-Container-1">
+                {props.windowWidth <= 740 ? (
+                    <div>
+                        <LandingHeadshot />
+                        <div className="Spacer-1X" />
+                    </div>
+                ) : (
+                    <></>
+                )}
+                <LandingHeader windowWidth={props.windowWidth} />
+                <div className="Spacer-2X" />
+                <LandingSubheader />
+                <div className="Spacer-4X" />
+                <LandingWorkSection />
+                <div className="Spacer-1X" />
+                <LandingCausesSection />
+                <div className="Spacer-1X" />
+                <LandingEducationSection />
+                <div className="Spacer-1X" />
+                <LandingCVSection />
+                <div className="Spacer-1X" />
+                <LandingTalkSection />
+            </div>
+        </>
     );
 }
