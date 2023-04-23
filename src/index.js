1e7db2a8e85f8fd551fb7cfeeaff325d4922cfd5
jwpettit <jwpettit@me.com>
2023-04-23 00:21:26 -0700

    commented out helmet

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
+// import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <BrowserRouter>
-        <HelmetProvider>
+        {/* <HelmetProvider> */}
             <App />
-        </HelmetProvider>
+        {/* </HelmetProvider> */}
     </BrowserRouter>
 );
