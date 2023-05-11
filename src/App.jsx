b845572f7038cdca48b3ef018615e2daa5b4bbcc
jwpettit <jwpettit@me.com>
2023-05-10 18:31:55 -0700

    Refactored all fetches into one big query

--- src/App.jsx
+++ src/App.jsx
@@ -40,84 +40,113 @@ export default function App() {
         };
     }, []);
 
+    // useEffect(() => {
+    //     const fetchAboutInfo = async () => {
+    //         const query =
+    //             '*[_type == "about"][0]{about, tagline, "avatarImageURL": avatarImage.asset->url, "avatarImageAlt": avatarImage.altText}';
+    //         try {
+    //             const result = await CMSClient.fetch(query);
+    //             setAboutInfo(result);
+    //         } catch (error) {
+    //             console.error("Error fetching about info:", error);
+    //         }
+    //     };
+
+    //     const fetchResumeJobs = async () => {
+    //         const query = '*[_type == "resume"][0].jobs';
+    //         try {
+    //             const result = await CMSClient.fetch(query);
+    //             setResumeJobs(result);
+    //         } catch (error) {
+    //             console.error("Error fetching resume jobs:", error);
+    //         }
+    //     };
+
+    //     const fetchResumeCauses = async () => {
+    //         const query = '*[_type == "resume"][0].causes';
+    //         try {
+    //             const result = await CMSClient.fetch(query);
+    //             setResumeCauses(result);
+    //         } catch (error) {
+    //             console.error("Error fetching resume causes:", error);
+    //         }
+    //     };
+
+    //     const fetchResumeEducation = async () => {
+    //         const query = '*[_type == "resume"][0].education';
+    //         try {
+    //             const result = await CMSClient.fetch(query);
+    //             setResumeEducation(result);
+    //         } catch (error) {
+    //             console.error("Error fetching resume education:", error);
+    //         }
+    //     };
+
+    //     const fetchContactInfo = async () => {
+    //         const query =
+    //             '*[_type == "contact"][0] {phone, email, mastodonHandle, mastodonURL}';
+    //         try {
+    //             const result = await CMSClient.fetch(query);
+    //             setContactInformation(result);
+    //         } catch (error) {
+    //             console.error("Error fetching contact information:", error);
+    //         }
+    //     };
+
+    //     const fetchResumePDF = async () => {
+    //         const query =
+    //             '*[_type == "resume"][0]{"resumeURL": downloadablePDF.asset->url}';
+    //         try {
+    //             const result = await CMSClient.fetch(query);
+    //             setResumePDFLink(result.resumeURL);
+    //         } catch (error) {
+    //             console.error("Error fetching resume PDF link:", error);
+    //         }
+    //     };
+
+    //     Promise.all([
+    //         fetchAboutInfo(),
+    //         fetchResumeJobs(),
+    //         fetchResumeCauses(),
+    //         fetchResumeEducation(),
+    //         fetchContactInfo(),
+    //         fetchResumePDF(),
+    //     ])
+    //         .then(() => {
+    //             setDataLoaded(true);
+    //         })
+    //         .catch((error) => {
+    //             console.error("Error fetching data:", error);
+    //         });
+    // }, []);
+
     useEffect(() => {
-        const fetchAboutInfo = async () => {
-            const query =
-                '*[_type == "about"][0]{about, tagline, "avatarImageURL": avatarImage.asset->url, "avatarImageAlt": avatarImage.altText}';
+        const fetchData = async () => {
+            const query = `
+            {
+                "aboutInfo": *[_type == "about"][0]{about, tagline, "avatarImageURL": avatarImage.asset->url, "avatarImageAlt": avatarImage.altText},
+                "resumeJobs": *[_type == "resume"][0].jobs,
+                "resumeCauses": *[_type == "resume"][0].causes,
+                "resumeEducation": *[_type == "resume"][0].education,
+                "contactInfo": *[_type == "contact"][0] {phone, email, mastodonHandle, mastodonURL},
+                "resumePDF": *[_type == "resume"][0]{"resumeURL": downloadablePDF.asset->url}
+            }
+        `;
             try {
                 const result = await CMSClient.fetch(query);
-                setAboutInfo(result);
-            } catch (error) {
-                console.error("Error fetching about info:", error);
-            }
-        };
-
-        const fetchResumeJobs = async () => {
-            const query = '*[_type == "resume"][0].jobs';
-            try {
-                const result = await CMSClient.fetch(query);
-                setResumeJobs(result);
-            } catch (error) {
-                console.error("Error fetching resume jobs:", error);
-            }
-        };
-
-        const fetchResumeCauses = async () => {
-            const query = '*[_type == "resume"][0].causes';
-            try {
-                const result = await CMSClient.fetch(query);
-                setResumeCauses(result);
-            } catch (error) {
-                console.error("Error fetching resume causes:", error);
-            }
-        };
-
-        const fetchResumeEducation = async () => {
-            const query = '*[_type == "resume"][0].education';
-            try {
-                const result = await CMSClient.fetch(query);
-                setResumeEducation(result);
-            } catch (error) {
-                console.error("Error fetching resume education:", error);
-            }
-        };
-
-        const fetchContactInfo = async () => {
-            const query =
-                '*[_type == "contact"][0] {phone, email, mastodonHandle, mastodonURL}';
-            try {
-                const result = await CMSClient.fetch(query);
-                setContactInformation(result);
-            } catch (error) {
-                console.error("Error fetching contact information:", error);
-            }
-        };
-
-        const fetchResumePDF = async () => {
-            const query =
-                '*[_type == "resume"][0]{"resumeURL": downloadablePDF.asset->url}';
-            try {
-                const result = await CMSClient.fetch(query);
-                setResumePDFLink(result.resumeURL);
-            } catch (error) {
-                console.error("Error fetching resume PDF link:", error);
-            }
-        };
-
-        Promise.all([
-            fetchAboutInfo(),
-            fetchResumeJobs(),
-            fetchResumeCauses(),
-            fetchResumeEducation(),
-            fetchContactInfo(),
-            fetchResumePDF(),
-        ])
-            .then(() => {
+                setAboutInfo(result.aboutInfo);
+                setResumeJobs(result.resumeJobs);
+                setResumeCauses(result.resumeCauses);
+                setResumeEducation(result.resumeEducation);
+                setContactInformation(result.contactInfo);
+                setResumePDFLink(result.resumePDF.resumeURL);
                 setDataLoaded(true);
-            })
-            .catch((error) => {
+            } catch (error) {
                 console.error("Error fetching data:", error);
-            });
+            }
+        };
+
+        fetchData();
     }, []);
 
     return (
